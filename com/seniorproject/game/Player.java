c3fbdc21338b107aed50e30bcba36b1450043750
jkrepps <jk109017@tamu.edu>
2014-10-26 13:32:55 -0500

    Update to Player
    Added the ability to interact with the database and buy and sell
    inventory.

--- com/seniorproject/game/Player.java
+++ com/seniorproject/game/Player.java
@@ -1,6 +1,10 @@
 package com.seniorproject.game;
-import java.util.Random;
 
+import com.seniorproject.dao.DaoObject;
+import java.util.Random;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 public class Player
 {
@@ -10,7 +14,15 @@ public class Player
     private String name;
 	private String password;
 	private int land;
+    private DaoObject dao;
+
     Random rand = new Random();
+
+
+
+    File logFile = new File("logfile.txt"); //not sure if this is the best place to define this! Also, need to be sure file actually exists, if not, do logFile.createNewFile()
+    FileWriter fw = new FileWriter(file.getAbsoluteFile());
+    BufferedWriter bw = new BufferedWriter(fw);
 
 	
     public Player(int g, String playerName, String p) { // initialization function
@@ -21,7 +33,7 @@ public class Player
 		password = p;
     }
     
-    int[] inventory= new int[NUMITEMS];//Item[] inventory= new Item[NUMITEMS];
+    Resource[] inventory= new int[NUMITEMS];//Item[] inventory= new Item[NUMITEMS];
     
 												/*The following are all standard object info retrieval functions.*/
     public int getGold() { return gold; }
@@ -35,40 +47,82 @@ public class Player
     public void recieve(int d) { gold += d; }
     public void pay(int d) { gold -= d; }
 
-    public void addItem(int item) //(Item item)
+    public void addResource(Resource resource)
 	{ 
+		//add to inventory array
 		for (int i=0;i<inventory.length;i++)
 			if (inventory[i]==0)
 			{
-				inventory[i]=item;	
+				inventory[i]=resource;	
 				System.out.println("new purchase");
 				break;
 			}
-			
+
+		//add to inventory database
+		dao.executeUpdate("INSERT INTO ResourceList VALUES (\"" + resource.getResourceName() + "\", \"" + resource.getResourceClass() + "\", \"" + resource.getResourceCost() + "\" );";	
 			
 	}
-	public void removeItem(int item)//(Item item) 
+
+	public void removeResource(Resource resource) 
 	{ 
 		for (int i=0;i<inventory.length;i++)
-			if (inventory[i]==item)
+			if (inventory[i]==resource)
 			{
 				inventory[i]=0;	
 				System.out.println("inventory slot removed");
 				break;
 			}
-			
+
+		//remove from inventory database (may need to make this more specific)
+		dao.executeDelete("DELETE FROM Inventory WHERE resourceName = " + resource.getResourceName);
 	}
-    public int[] getInventory() //Item[] getInventory() 
+
+
+    public Resource[] getInventory()
     { 
-    		return inventory;
+    	return inventory;
     }
 	
-    public boolean isInInventory(int item)//(Item item)
+    public boolean isInInventory(Resource resource)    
     {
 		for (int i=0;i<inventory.length;i++)
-			if (inventory[i]==item)	return true;
+			if (inventory[i]==resource)	return true;
 		return false;	
-    } 
+    }
+
+
+    //buy resource?
+	// array of three types of resource
+	// buy resource, then figure out which kind of resource, then what to do based on 
+	// could just take in the string of a 
+
+    public int[] buyResource(Resource resource) //for right now returns int, in future could be different
+	{
+		//subtract gold (price of resource)
+		gold = gold-resource.getResourceCost();
+
+		//add to inventory (add to database!)
+		inventory.addResource(resource);
+		
+		//publish to activity log
+		bw.write(player.getName() + " purchased " + resource.getName());
+		bw.close();
+	}
+
+	//are we doing this?
+	public int[] sellResource(Resource resource)
+	{
+		//add gold (profit from resource, for right now is just the price of resource)
+		//figure out how to make market work
+		gold = gold + resource.getResourceCost();
+
+		//remove from inventory (remove from database!)
+		inventory.removeResource(resource);
+
+		//publish to activity log
+		//not logging name of player purchasing because it will say they purchased it
+		//should we specify whether purchasing secondhand?
+		bw.write(player.getName() + " sold " + resource.getResourceName());
+	}
 	
-   
 }
