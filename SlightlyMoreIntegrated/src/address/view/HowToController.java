9ca3644b013a9aecd65ca76727b2c33fa9a95cd1
jleroy1050 <jleroy1050@tamu.edu>
2014-11-04 16:03:36 -0600

    Polishing
    Just adding some polishing affects to the rest of the buttons (shadows
    and sounds :D )

--- SlightlyMoreIntegrated/src/address/view/HowToController.java
+++ SlightlyMoreIntegrated/src/address/view/HowToController.java
@@ -3,6 +3,7 @@ package address.view;
 import address.MainApplication;
 import javafx.fxml.*;
 import javafx.scene.control.*;
+import javafx.scene.media.AudioClip;
 
 public class HowToController implements ControlledScreen {
 	/*-------------------------------------------*/
@@ -11,6 +12,10 @@ public class HowToController implements ControlledScreen {
 	private ScreensController myController;
 	@FXML
 	private Button back;
+	java.net.URL buttonSound = getClass().getResource("./Sounds/buttons.aiff");
+	java.net.URL buttonSound2 = getClass().getResource("./Sounds/buttonSound.mp3");
+	private AudioClip buttonDrag =  new AudioClip(buttonSound.toString());
+	private AudioClip buttonClick = new AudioClip(buttonSound2.toString());
 	
 	/*-------------------------------------------*/
 	/*			HELPER FUNCTIONS				 */
