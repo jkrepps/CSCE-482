aefb36bfaf8b913e63aa5e834164ba7bfb4d3022
jkrepps <jk109017@tamu.edu>
2014-11-05 01:00:51 -0600

    one more small change to the logger

--- com/seniorproject/logger/Logger.java
+++ com/seniorproject/logger/Logger.java
@@ -8,49 +8,45 @@ import java.io.*;
 
 
 public class Logger {
+  private File logFile = new File("com/seniorproject/logFile.txt");
 
-	private File logFile = new File("com/seniorproject/logFile.txt");
-
-    public void writeToLog(String string) {
+  public void writeToLog(String string) {
 		try {
-   			BufferedWriter bw = new BufferedWriter(new FileWriter(logFile.getAbsoluteFile(), true));
-   			bw.newLine();
-   			bw.write(string);
-   			bw.close();
+   		BufferedWriter bw = new BufferedWriter(new FileWriter(logFile.getAbsoluteFile(), true));
+   		bw.newLine();
+   		bw.write(string);
+   		bw.close();
 		} catch (Exception e1) {
 			System.err.println(e1.getMessage());
 		} 
-    }
+  }
 
-    public int getNumberLines(){
-    	try {
-    		LineNumberReader lnr = new LineNumberReader(new FileReader(logFile));
+  public int getNumberLines(){
+    try {
+    	LineNumberReader lnr = new LineNumberReader(new FileReader(logFile));
 			lnr.skip(Long.MAX_VALUE);
 			int temp = lnr.getLineNumber() + 1;
 			lnr.close();
-			return temp;
-			
+			return temp;	
 		} catch (Exception e){
 			System.err.println(e.getMessage());
 		}
 		return 0;
-    }
+  }
 
-    public String readFromLog() {
-    	String log = "";
-    	try {
-    		BufferedReader br = new BufferedReader(new FileReader(logFile));
+  public String readFromLog() {
+    String log = "";
+    try {
+    	BufferedReader br = new BufferedReader(new FileReader(logFile));
  			String line = null;
-
  			while ((line = br.readLine()) != null) 
  			{
-   				log += line + "\n";
+   			log += line + "\n";
  			}
  			return log;
- 		}
- 		catch (Exception e) {
+ 		} catch (Exception e) {
  			System.err.println(e.getMessage());
  		}
  		return null;
-    }
+  }
 }
