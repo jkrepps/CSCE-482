a75cdb5d101c347684014747e772d7c3b45327a8
jkrepps <jk109017@tamu.edu>
2014-12-08 19:50:36 -0600

    actually fixed mindnight lol

--- com/seniorproject/game/World.java
+++ com/seniorproject/game/World.java
@@ -16,7 +16,7 @@ public class World
 	
 	private enum daytimes{
 	
-	MORNING, MIDDAY, AFTERNOON, EVENING, NIGHT, MINDNIGHT, EARLYMORNING;
+	MORNING, MIDDAY, AFTERNOON, EVENING, NIGHT, MIDNIGHT, EARLYMORNING;
 	
 		public daytimes next()
 	    {
